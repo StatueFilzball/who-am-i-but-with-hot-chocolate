7eac835e882f96850cf8d9d5fa5b6076c26dfc9d
Jan Auer <janauer2@gmx.de>
2022-12-05 12:03:12 +0800

    first iteration HTML

---
+++ js/classCharacter.js
@@ -0,0 +1,12 @@
+//creating the class character
+
+class Character{
+    constructor(isFemale, age, hasEyewear, hasHeadwear, billboardHot100){
+        this.isFemale = isFemale,
+        this.age = age
+        this.hasEyewear = hasEyewear,
+        this.hasHeadwear = hasHeadwear,
+        this.billboardHot100 = billboardHot100
+    }
+}
+
