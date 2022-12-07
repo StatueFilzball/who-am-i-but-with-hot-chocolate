70e5fbcccd2683d4bfd34f2828b222af75b3daa8
Jan Auer <janauer2@gmx.de>
2022-12-07 23:04:30 +0800

    improve endgame styling

--- js/index.js
+++ js/index.js
@@ -42,35 +42,36 @@ let guessCount = 0
 const yourIdentity = characterArray[Math.floor(Math.random()*characterArray.length)]
 console.log("The Winner Pick", yourIdentity)
 const imgElements = document.querySelectorAll("img")
-
+const guessboxElement = document.getElementById("guess-box")
+const instructionsElement = document.getElementById("instruction")
 
 function selectorManipulator(buttonInput) {
 
 if(buttonInput === 1){
-document.getElementById("guess-box").innerHTML = `<p>I think I am
+    guessboxElement.innerHTML = `<p>I think I am
 <button class="button-style" onclick="valueCheckerSex(true)">Female</button>
 <button class="button-style" onclick="valueCheckerSex(false)">Male</button></p>`
 }
 
 if(buttonInput === 2){
-document.getElementById("guess-box").innerHTML = `<p>I think I am between
+    guessboxElement.innerHTML = `<p>I think I am between
 <span><input type="number" id="firstValue"></input><span> and <span><input type="number" id="secondValue"></input><span> years old.</p> <button class="button-style" onclick="valueCheckerAge()">Hit it</button>`
 }
 
 if(buttonInput === 3){
-document.getElementById("guess-box").innerHTML = `<p>I think I am
+    guessboxElement.innerHTML = `<p>I think I am
 <button class="button-style" onclick="valueCheckerEyewear(true)">wearing glasses</button>
 <button class="button-style" onclick="valueCheckerEyewear(false)">not wearing glasses</button></p>`
 }
 
 if(buttonInput === 4){
-document.getElementById("guess-box").innerHTML = `<p>I think I am
+    guessboxElement.innerHTML = `<p>I think I am
 <button class="button-style" onclick="valueCheckerHeadwear(true)">wearing headwear</button>
 <button class="button-style" onclick="valueCheckerHeadwear(false)">not wearing headwear</button></p>`
 }
 
 if(buttonInput === 5){
-document.getElementById("guess-box").innerHTML = `<p>I think I had between
+    guessboxElement.innerHTML = `<p>I think I had between
 <span><input type="number" id="firstValueBillBoard"></input><span> and <span><input type="number" id="secondValueBillboard"></input><span> hits in the Billboard Hot 100.</p> <button class="button-style" onclick="valueCheckerBillboard()">Hit it</button><br><p>Note: The Billboard Hot 100 is the music industry standard record chart in the United States for songs, published weekly by Billboard magazine. Chart rankings are based on sales (physical and digital), radio play, and online streaming in the United States.<p>`
 }
 
@@ -122,13 +123,8 @@ pictureSwapper()
     console.log("Updated array",characterArray)
     guessCounter()
     guessProbability()
-    if(characterArray.length === 1){
-        endGame()
-    } else{
-        renderGuessbox()
-    }
+    endGame()
 }
-
 
 function valueCheckerHeadwear(value){
 
@@ -151,14 +147,10 @@ function valueCheckerHeadwear(value){
     console.log("Updated array",characterArray)
     guessCounter()
     guessProbability()
-    if(characterArray.length === 1){
-        endGame()
-    } else{
-        renderGuessbox()
-    }
+    endGame()
+    
     
 }
-
 
 function valueCheckerAge(){
 
@@ -204,15 +196,10 @@ else if(lowValue > yourIdentity.age || highValue < yourIdentity.age){
     guessCounter()
     guessProbability()
     console.log("Updated array",characterArray)
-    if(characterArray.length === 1){
-        endGame()
-    } else{
-        renderGuessbox()
-    }
+    endGame()
     
 
 }
-
 
 function valueCheckerBillboard(){
 
@@ -257,26 +244,16 @@ else if(lowValue > yourIdentity.billboardHot100 || highValue < yourIdentity.bill
     console.log("Updated array",characterArray)
     guessCounter()
     guessProbability()
-    if(characterArray.length === 1){
-        endGame()
-    } else{
-        renderGuessbox()
-    }
+    endGame()
 }
 
-function endGame(){
-
-document.getElementById("instruction").innerHTML = `<p>Damn champ, you figured it out! You are ${yourIdentity.name}!</p><br><img src=${yourIdentity.colorImage}><br><p>Cool little fun fact: You are way better at DOM Manipulation than ${yourIdentity.name} ever will be. Keep rocking the Bootcamp, you little superstar!</p><br><button class="button-style" onClick="history.go(0);">Play again</button>`
-document.getElementById("instruction").classList = "endGameFlex"
-
-}
 
 function theCheekyRabbit(){
-    document.getElementById("guess-box").innerHTML = `<h2>Wait, a cute bunny?!</h2><br><div id="bunny-box-full"><div id="bunny-box"><p>Hey there, can you believe this weather? It's freezing out there. Before your next guess, take this hot chocolate and get comfy.</p><div id="bunny-buttons"><button class="button-style" onclick="renderGuessbox()">No thanks, cute bunny.</button><button class="button-style" onclick="gameOver()">Lovely. I'll take it</button></div></div><br><img src=./images/images-color/hot-chocolate-transparent.png><img src=./images/images-color/cute-bunny-3.png></div>`
+    guessboxElement.innerHTML = `<h2>Wait, a cute bunny?!</h2><br><div id="bunny-box-full"><div id="bunny-box"><p>Hey there, can you believe this weather? It's freezing out there. Before your next guess, take this hot chocolate and get comfy.</p><div id="bunny-buttons"><button class="button-style" onclick="renderGuessbox()">No thanks, cute bunny.</button><button class="button-style" onclick="gameOver()">Lovely. I'll take it</button></div></div><br><img src=./images/images-color/hot-chocolate-transparent.png><img src=./images/images-color/cute-bunny-3.png></div>`
     }
 
 function renderGuessbox(){             
-    document.getElementById("guess-box").innerHTML = `<h2>Take your guess!</h2><p>I got a feeling I know stuff about my</p>
+    guessboxElement.innerHTML = `<h2>Take your guess!</h2><p>I got a feeling I know stuff about my</p>
     <div id="renderGuessbox"><button class="button-style" onclick="selectorManipulator(1)">Sex</button>
     <button class="button-style" onclick="selectorManipulator(2)">Age</button>
     <button class="button-style" onclick="selectorManipulator(3)">Eyewear</button>
@@ -286,17 +263,17 @@ function renderGuessbox(){
 }
 
 function gameOver(){
-    document.getElementById("instruction").innerHTML = `<p>You lost. You really did not read the instructions, did you :(</p><button class="button-style" onClick="history.go(0);">Try again</button>`
-    document.getElementById("instruction").classList += "gameOver"
+    instructionsElement.innerHTML = `<p>You lost. You really did not read the instructions, did you :(</p><button class="button-style" onClick="history.go(0);">Try again</button>`
+    instructionsElement.style.justifyContent = "center"
+    instructionsElement.style.alignItems = "center"
 }
 
 function identityGuess(){
-console.log("identityCheck triggered")
     let inputStr = document.getElementById("myIdentityGuess").value
     if (inputStr === yourIdentity.name){
         endGame()
     } else{
-        gameOver()
+        wrongGuess()
     }
 }
 
@@ -307,12 +284,9 @@ document.getElementById("counter").innerText = `${guessCount}`
 } 
 
 function guessProbability(){
-    console.log("Guess probability triggered")
     let winningChance = Math.floor(100/ characterArray.length)
-    console.log("Math floor chance", winningChance)
     document.getElementById("guess-probability").innerHTML = `${winningChance}`
 }
-
 
 function pictureSwapper(){
     for(let j = 0; j < imgElements.length; j++){
@@ -320,4 +294,20 @@ function pictureSwapper(){
           imgElements[j].src = characterToSwapImage.bwImage
       }
   }
-  }
+}
+
+function endGame(){
+    if(characterArray.length === 1){
+
+    document.getElementById("instruction").innerHTML = `<p>Damn champ, you figured it out! You are ${yourIdentity.name}!</p><br><img src=${yourIdentity.colorImage}><br><p>Cool little fun fact: You are way better at DOM Manipulation than ${yourIdentity.name} ever will be. Keep rocking the Bootcamp, you little superstar!</p><br><button class="button-style" onClick="history.go(0);">Play again</button>`
+    document.getElementById("instruction").classList = "endGameFlex"
+    } else {
+        renderGuessbox()
+    }
+    }
+
+function wrongGuess(){
+    instructionsElement.innerHTML = `<p>Today ain't your lucky day :(</p><br><button class="button-style" onClick="history.go(0);">Try again</button>`
+    instructionsElement.style.justifyContent = "center"
+    instructionsElement.style.alignItems = "center"
+}
